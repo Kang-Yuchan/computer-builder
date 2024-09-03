0f0674eb3348c446da1356215f32029ff0cf66e7
Kang-Yuchan <mikuplayer123@gmail.com>
2024-09-04 04:06:42 +0900

    feat: Add cpu select section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import Header from "./components/header"
+import CustomSelect from './components/custom-select';
+import Header from './components/header';
+import Section from './components/section';
+import { useCpu } from './hooks/useCpu';
 
 function App() {
+  const {
+    cpuBrandsOptions,
+    cpuModelOptions,
+    setSelectedBrand,
+    setSelectedCpuModel,
+  } = useCpu();
   return (
-   <>
-	 	<Header />
-		<main>
-			Computer Builder
-		</main>
-	 </>
-  )
+    <>
+      <Header />
+      <main>
+        <Section title="step1: Select Your CPU">
+          <div className="ml-2 py-2 flex gap-4">
+            <CustomSelect
+              options={cpuBrandsOptions}
+              label="Brand"
+              onChange={setSelectedBrand}
+            />
+            <CustomSelect
+              options={cpuModelOptions}
+              label="Model"
+              onChange={setSelectedCpuModel}
+            />
+          </div>
+        </Section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
